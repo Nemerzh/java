c84b437ec791b995cb1738fb497c2391f90517bf
Nemerzh <Anulvl001@gmail.com>
2023-02-13 22:30:36 +0200

    2

--- untitled/src/Employee.java
+++ untitled/src/Employee.java
@@ -1,29 +1,51 @@
 public class Employee {
     private String firstName;
     private String lastName;
-    private int stavka;
+    private int salaryPerHours;
     private int countDay;
 
     public Employee(String firstName, String lastName, int stavka, int countDay) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.stavka = stavka;
+        this.salaryPerHours = stavka;
         this.countDay = countDay;
     }
 
     public int calculateSalary() {
         int salary;
-        salary = stavka * countDay;
-        return salary;
+        return salary = salaryPerHours * countDay;
     }
 
     public int compare(Employee a) {
-        int b = 0;
-        if (this.stavka * this.countDay > a.countDay* a.stavka) {
-            b = 1;
-        } else if (this.stavka * this.countDay < a.countDay* a.stavka) {
-            b = -1;
-        } else b = 0;
-        return b;
+        if (this.calculateSalary() > a.calculateSalary()) {
+            return 1;
+        } else if (this.calculateSalary() < a.calculateSalary()) {
+            return  -1;
+        } else return 0;
     }
+
+    public int workingDay() {
+        return this.countDay;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setCountDay(int countDay) {
+        this.countDay = countDay;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
 }
--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!1");
+        Employee worker1 = new Employee("Sasha", "Nev", 25, 20);
+        Employee worker2 = new Employee("Pasha", "Low", 23, 19);
+        System.out.println(worker1.getFirstName() + " " + worker1.getLastName() + " working days is " + worker1.workingDay());
+        System.out.println(worker2.getFirstName() + " " + worker2.getLastName() + " working days is " + worker2.workingDay());
+        if (worker1.compare(worker2) == 1) {
+            System.out.println(worker1.getFirstName() + " earn more than " + worker2.getFirstName());
+        } else if (worker1.compare(worker2) == -1) {
+            System.out.println(worker2.getFirstName() + " earn more than " + worker1.getFirstName());
+        } else System.out.println(worker1.getFirstName() + " earns as much as " + worker2.getFirstName());
     }
 }
